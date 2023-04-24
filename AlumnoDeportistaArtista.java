d5c8a6af69c070d9ea1251a743962160da4e4dbc
GabrielaCedillo <119831781+GabrielaCedillo@users.noreply.github.com>
2023-04-24 12:55:56 -0600

    Add files via upload

---
+++ AlumnoDeportistaArtista.java
@@ -0,0 +1,75 @@
+package org.example.herenciamultiple;
+
+public class AlumnoDeportistaArtista extends Alumno implements Deportista,Artista{
+    private int idActividiExtra;
+    private String deporte;
+    private String arte;
+    public AlumnoDeportistaArtista() {
+    }
+
+    public AlumnoDeportistaArtista(String nombre, int edad, int numeroCuenta, String carrera, double promedio, int idActiviExtra, String deporte, String arte) {
+        super(nombre, edad, numeroCuenta, carrera, promedio);
+        this.idActiviExtra = idActiviExtra;
+        this.deporte = deporte;
+        this.arte = arte;
+    }
+
+    public int getIdActiviExtra() {
+        return idActiviExtra;
+    }
+
+    public void setIdActiviExtra(int idActiviExtra) {
+        this.idActiviExtra = idActiviExtra;
+    }
+
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+
+    public String getArte() {
+        return arte;
+    }
+
+    public void setArte(String arte) {
+        this.arte = arte;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "AlumnoDeportistaArtista{" +
+                "idActiviExtra=" + idActiviExtra +
+                ", deporte='" + deporte + '\'' +
+                ", arte='" + arte + '\'' +
+                '}';
+    }
+
+    @Override
+    public int ensayar(String disciplina) {
+        System.out.println("El alumno esta ensayando " + this.arte);
+        return 2;
+    }
+
+    @Override
+    public boolean presentarObra() {
+        System.out.println(getNombre() + " Esta presentando su obra...");
+        return true;
+    }
+
+    @Override
+    public int entrenar() {
+        System.out.println(getNombre() + " esta entrenando el deporte " + deporte);
+        return 2;
+    }
+
+    @Override
+    public boolean presentarCompetencia(String ubicacion) {
+        System.out.println(getNombre() + " esta en competencia del deporte " + deporte);
+        return true;
+    }
+}
+
---
+++ Artista.java
@@ -0,0 +1,7 @@
+package org.example.herenciamultiple;
+
+public interface Artista {
+    public int ensayar(String disciplina);
+    public boolean presentarObra();
+
+}
